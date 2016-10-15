14301e73a25550818d6e3ae1e6effccd68a20860
fanlizhou <lizhou.fan@midea.com>
2016-10-15 18:17:07 +0800

    create Server class

---
+++ sven/api/wsgi.py
@@ -0,0 +1,14 @@
+import os
+import asyncio
+
+from aiohttp import web
+
+
+class Server(object):
+    """Server class that handle service
+
+    manage service
+    """
+
+    def __init__(self, loop):
+        pass
