e7c374ba52002e1e30272150fcf453be3e07901d
fanlizhou <lizhou.fan@midea.com>
2016-10-20 17:36:55 +0800

    use app.{decorator} to add route info

--- sven/cmd/api.py
+++ sven/cmd/api.py
@@ -1,53 +1,53 @@
-import asyncio
-
 from sven.api import wsgi
-from aiohttp import web
+from sven.api.wsgi import Response
 
 
+app = wsgi.Application()
+
+
+@app.get('/get/{id}')
 async def get_func_1(id):
-    return web.Response(text=id)
+    return Response(text=id)
 
+
+@app.get('/get')
 async def get_func_2():
-    return web.Response(text="get func without parameters")
+    return Response(text="get func without parameters")
 
-async def post_func_1(id):
-    return web.Response(text=id)
 
-async def post_func_2():
-    return web.Response(text='post func without parameters')
-
-async def delete_func_1(id, name):
+@app.post('/post/{id}')
+async def post_func_1(id, name):
     resp = id + name
-    return web.Response(text=resp)
+    return Response(text=resp)
 
+
+@app.post('/post')
+async def post_func_2():
+    return Response(text='post func without parameters')
+
+
+@app.delete('/delete/{id}')
+async def delete_func_1(id):
+    resp = id
+    return Response(text=resp)
+
+
+@app.delete('/delete')
 async def delete_func_2():
-    return web.Response(text='delete func without parameters')
+    return Response(text='delete func without parameters')
 
+
+@app.put('/put/{id}')
 async def put_func_1(id, name):
     resp = id + name
-    return web.Response(text=resp)
+    return Response(text=resp)
 
+
+@app.put('/put')
 async def put_func_2():
-    return web.Response(text='put func without parameters')
-
-
-def main():
-    loop = asyncio.get_event_loop()
-    router = wsgi.Router()
-    router.add_route('GET', '/get', wsgi.RequestHandler(get_func_2))
-    router.add_route('GET', '/get/{id}', wsgi.RequestHandler(get_func_1))
-    router.add_route('POST', '/post', wsgi.RequestHandler(post_func_2))
-    router.add_route('POST', '/post/{id}', wsgi.RequestHandler(post_func_1))
-    router.add_route('DELETE', '/delete', wsgi.RequestHandler(delete_func_2))
-    router.add_route('DELETE', '/delete/{id}', wsgi.RequestHandler(delete_func_1))
-    router.add_route('PUT', '/put', wsgi.RequestHandler(put_func_2))
-    router.add_route('PUT', '/put/{id}', wsgi.RequestHandler(put_func_1))
-
-    app = wsgi.Application(loop)
-    app._router = router
-    server = wsgi.Server(app)
-    server.run('0.0.0.0', 8000)
+    return Response(text='put func without parameters')
 
 
 if __name__ == '__main__':
-    main()
+    server = wsgi.Server(app)
+    server.run('0.0.0.0', 8000)
