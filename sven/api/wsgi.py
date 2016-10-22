83b2a835cd05f049bdb97d005f2a8ba209133325
fanlizhou <lizhou.fan@midea.com>
2016-10-22 15:33:54 +0800

    add template engine

--- sven/api/wsgi.py
+++ sven/api/wsgi.py
@@ -5,6 +5,13 @@ from urllib import parse
 from json.decoder import JSONDecodeError
 
 from aiohttp import web
+from jinja2 import Environment, FileSystemLoader
+
+# test code for config
+
+config = dict(
+    templates_path='E:/Python_Project/templates'
+)
 
 
 def has_request(func):
@@ -12,10 +19,31 @@ def has_request(func):
     return True if "request" in parameters.keys() else False
 
 
+def init_template_engine(templates_path, filters=None,
+                         autoescape=True, block_start_string='{%',
+                         block_end_string='%}', variable_start_string='{{',
+                         variable_end_string='}}', auto_reload=True):
+    options = dict(
+        autoescape=autoescape,
+        block_start_string=block_start_string,
+        block_end_string=block_end_string,
+        variable_start_string=variable_start_string,
+        variable_end_string=variable_end_string,
+        auto_reload=auto_reload
+    )
+    env = Environment(loader=FileSystemLoader(templates_path), **options)
+    if filters is not None and isinstance(filters, dict):
+        for name, func in filters.items():
+            env.filters[name] = func
+
+    return env
+
+
 class Server(object):
     """Server class that handle service
     manage service
     """
+
     def __init__(self, app):
         self._app = app
 
@@ -28,55 +56,73 @@ class Application(web.Application):
 
     wsgi application class.
     """
+
     def __init__(self, loop=None):
+        self.template_engine = init_template_engine(config['templates_path'])
         super().__init__(loop=loop)
 
     def get(self, path):
         """
         define app.get('/path')
+        :param path: restful url
         """
+
         def decorator(func):
             @functools.wraps(func)
             def wrapper(*args, **kwargs):
                 return func(*args, **kwargs)
+
             self.router.add_route('GET', path, RequestHandler(wrapper))
             return wrapper
+
         return decorator
 
     def post(self, path):
         """
         define app.post('/path')
+        :param path: restful url
         """
+
         def decorator(func):
             @functools.wraps(func)
             def wrapper(*args, **kwargs):
                 return func(*args, **kwargs)
+
             self.router.add_route('POST', path, RequestHandler(wrapper))
             return wrapper
+
         return decorator
 
     def put(self, path):
         """
         define app.post('/path')
+        :param path: restful url
         """
+
         def decorator(func):
             @functools.wraps(func)
             def wrapper(*args, **kwargs):
                 return func(*args, **kwargs)
+
             self.router.add_route('PUT', path, RequestHandler(wrapper))
             return wrapper
+
         return decorator
 
     def delete(self, path):
         """
         define app.post('/path')
+        :param path: restful url
         """
+
         def decorator(func):
             @functools.wraps(func)
             def wrapper(*args, **kwargs):
                 return func(*args, **kwargs)
+
             self.router.add_route('DELETE', path, RequestHandler(wrapper))
             return wrapper
+
         return decorator
 
     def copy(self):
@@ -119,7 +165,6 @@ class RequestHandler(object):
 
 
 class Response(web.Response):
-
     def __init__(self, status=200, headers=None,
                  content_type=None, charset=None,
                  body=None, text=None):
