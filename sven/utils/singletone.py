4be3eacb208bad69c482c2e7beae4206d508d4ff
fanlizhou <lizhou.fan@midea.com>
2016-10-26 11:51:18 +0800

    finish singletone

--- sven/utils/singletone.py
+++ sven/utils/singletone.py
@@ -2,6 +2,7 @@ def singleton(cls):
     instances = {}
 
     def _singleton(*args, **kw):
+
         if cls not in instances:
             instances[cls] = cls(*args, **kw)
         return instances[cls]
