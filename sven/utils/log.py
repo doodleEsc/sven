10aafed1c5290592ec9a0ca45c83ddaf59b5320e
fanlizhou <lizhou.fan@midea.com>
2016-10-28 15:06:18 +0800

    finish log module

--- sven/utils/log.py
+++ sven/utils/log.py
@@ -4,22 +4,60 @@ log module
 
 import os
 import logging
+import logging.handlers
 
 from sven.utils.singletone import singleton
+from sven.api.exception.path import PathNotFoundError
+
+NORMAL_FILE = 'E:/log/access.log'
+ERROR_FILE = 'E:/log/error.log'
+LOG_PATH = 'E:/log'
 
 
 @singleton
 class Log(object):
 
     def __init__(self):
-        pass
+        self._init_env()
 
     def _init_env(self):
-        NORMAL_FILE = 'E:/log/access.log'
-        ERROR_FILE = 'E:/log/error.log'
         try:
-            path_exist = os.path.exists('E:/log')
+            path_exist = os.path.exists(LOG_PATH)
             if not path_exist:
-                os.mkdir('E:/log')
+                os.mkdir(LOG_PATH)
         except OSError:
-            pass
+            raise PathNotFoundError("Paht %s Not Found" %LOG_PATH)
+
+        error_handler = logging.handlers.RotatingFileHandler(ERROR_FILE, maxBytes=1024*1024, backupCount=3)
+        error_handler.setLevel(logging.ERROR)
+
+        normal_handler = logging.handlers.RotatingFileHandler(NORMAL_FILE, maxBytes=1024*1024, backupCount=3)
+        normal_handler.setLevel(logging.DEBUG)
+
+        fmt_str = '%(asctime)s %(filename)s:%(lineno)s %(levelname)-8s [-] %(message)s'
+        fmt = logging.Formatter(fmt_str)
+
+        error_handler.setFormatter(fmt)
+        normal_handler.setFormatter(fmt)
+
+        self.logger = logging.getLogger('sven')
+        self.logger.addHandler(normal_handler)
+        self.logger.addHandler(error_handler)
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def warn(self, msg):
+        self.logger.warn(msg)
+
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+
+logger = Log()
